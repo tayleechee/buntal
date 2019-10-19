8e2f1ab112b7547fdc9e94aeb592af79433cdf6d
Elvin <hahaelvin@gmail.com>
2019-10-19 21:29:50 +0800

    Add missing dependencies & bug fixing.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-     <script src="{{ asset('js/app.js') }}" defer></script>
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8"></script>
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -163,10 +162,6 @@
         <main class="py-4">
             @yield('content')
         </main>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8"></script>
-          {!! $chart->script() !!}
-          {!! $chart2->script() !!}
-          {!! $chart3->script() !!}
     </div>
 
     <div id="errorModal" class="modal fade">
---
+++ storage/framework/views/0a0055a18981e82cd7caee73b7a4e8d53cf1e0bc.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Buntal IS</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            /*.links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }*/
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+           <!--  <?php if(Route::has('login')): ?>
+                <div class="top-right links">
+                    <?php if(auth()->guard()->check()): ?>
+                        <a href="<?php echo e(url('/home')); ?>">Home</a>
+                    <?php else: ?>
+                        <a href="<?php echo e(route('login')); ?>">Login</a>
+
+                        <?php if(Route::has('register')): ?>
+                            <a href="<?php echo e(route('register')); ?>">Register</a>
+                        <?php endif; ?>
+                    <?php endif; ?>
+                </div>
+            <?php endif; ?> -->
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Buntal Information System
+                </div>
+
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="card">
+                                <div class="card-header">
+                                    Admin Login
+                                </div>
+                                <div class="card-body">
+                                    <form method="POST" action="<?php echo e(route('login')); ?>">
+                                        <?php echo csrf_field(); ?>
+
+                                        <div class="form-group row">
+                                            <label for="email" class="col-md-4 col-form-label text-md-right"><?php echo e(__('E-Mail Address')); ?></label>
+
+                                            <div class="col-md-6">
+                                                <input id="email" type="email" class="form-control <?php if ($errors->has('email')) :
+if (isset($message)) { $messageCache = $message; }
+$message = $errors->first('email'); ?> is-invalid <?php unset($message);
+if (isset($messageCache)) { $message = $messageCache; }
+endif; ?>" name="email" value="<?php echo e(old('email')); ?>" required autocomplete="email" autofocus>
+
+                                                <?php if ($errors->has('email')) :
+if (isset($message)) { $messageCache = $message; }
+$message = $errors->first('email'); ?>
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong><?php echo e($message); ?></strong>
+                                                    </span>
+                                                <?php unset($message);
+if (isset($messageCache)) { $message = $messageCache; }
+endif; ?>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="password" class="col-md-4 col-form-label text-md-right"><?php echo e(__('Password')); ?></label>
+
+                                            <div class="col-md-6">
+                                                <input id="password" type="password" class="form-control <?php if ($errors->has('password')) :
+if (isset($message)) { $messageCache = $message; }
+$message = $errors->first('password'); ?> is-invalid <?php unset($message);
+if (isset($messageCache)) { $message = $messageCache; }
+endif; ?>" name="password" required autocomplete="current-password">
+
+                                                <?php if ($errors->has('password')) :
+if (isset($message)) { $messageCache = $message; }
+$message = $errors->first('password'); ?>
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong><?php echo e($message); ?></strong>
+                                                    </span>
+                                                <?php unset($message);
+if (isset($messageCache)) { $message = $messageCache; }
+endif; ?>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row mb-0">
+                                            <div class="col-md-8 offset-md-4 text-left">
+                                                <button type="submit" class="btn btn-primary">
+                                                    <?php echo e(__('Login')); ?>
+
+                                                </button>
+
+                                                <?php if(Route::has('password.request')): ?>
+                                                    <a class="btn btn-link" href="<?php echo e(route('password.request')); ?>">
+                                                        <?php echo e(__('Forgot Your Password?')); ?>
+
+                                                    </a>
+                                                <?php endif; ?>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- <div class="links">
+                    <a href="" class="btn btn-secondary">Form Insertion</a>
+                </div> -->
+                <div class="mt-4">
+                    Not admin? Fill form intead? <a href="<?php echo e(route('fillForm.step1')); ?>">Fill form</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+<?php /**PATH C:\Users\Elvin\Desktop\buntal\resources\views/welcome.blade.php ENDPATH**/ ?>
