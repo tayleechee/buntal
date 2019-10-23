daa46646ad372cfd778a0da8551d2cb06be75e0a
Elvin <hahaelvin@gmail.com>
2019-10-23 09:39:29 +0800

    Edit villager detail function polishing.

--- storage/framework/views/b12891ef92365a040aab880a5509fcc412dc8787.php
+++ storage/framework/views/b12891ef92365a040aab880a5509fcc412dc8787.php
@@ -54,7 +54,7 @@
 
 			<div class="form-group row pl-2 mt-3">
 				<label for="name" class="col-form-label col-2 form_name_label">Name</label>
-				<div class="col form-input-col">
+				<div class="col form-input-col d-none">
 					<input type="text" name="name" id="name" class="form-control form_name" value="<?php echo isset($villager->name) ? $villager->name : ''  ?>" required>
 				</div>
 				<div class="col form-input-div text-left col-form-label">
@@ -64,7 +64,7 @@
 
 			<div class="form-group row pl-2 mt-3">
 				<label for="ic" class="col-form-label col-2 form_ic_label">IC</label>
-				<div class="col form-input-col">
+				<div class="col form-input-col d-none">
 					<input type="text" name="ic" id="ic" class="form-control form_ic" value="<?php echo isset($villager->ic) ? $villager->ic : ''  ?>" required>
 				</div>
 				<div class="col form-input-div text-left col-form-label">
@@ -74,7 +74,7 @@
 
 			<div class="form-group row pl-2 mt-3">
 				<label for="gender" class="col-form-label col-2 form_gender_label">Gender</label>
-				<div class="col form-input-col">
+				<div class="col form-input-col d-none">
 					<select name="gender" id="gender" class="form-control form_gender" required>
 						<?php if(isset($villager->gender) && ($villager->gender == 'm')): ?>
 						<option value="male" selected>Male</option>
@@ -106,7 +106,7 @@
 
 			<div class="form-group row pl-2 mt-3">
 				<label for="dob" class="col-form-label col-2 form_dob_label">Date of Birth</label>
-				<div class="col form-input-col">
+				<div class="col form-input-col d-none">
 					<input type="date" name="dob" id="dob" class="form-control form_dob" value="<?php echo isset($villager->dob) ? $villager->dob : ''  ?>" required>
 				</div>
 				<div class="col form-input-div text-left col-form-label">
@@ -121,7 +121,7 @@
 
 			<div class="form-group row pl-2 mt-3">
 				<label for="race" class="col-form-label col-2 form_race_label">Race</label>
-				<div class="col form-input-col">
+				<div class="col form-input-col d-none">
 					<!-- <select name="race" id="race" class="form-control form_race" required>
 						<option value="malay">Malay</option>
 						<option value="cina">Cina</option>
@@ -139,7 +139,7 @@
 
 			<div class="form-group row pl-2 mt-3">
 				<label for="marital" class="col-form-label col-2 form_marital_label">Marital Status</label>
-				<div class="col form-input-col">
+				<div class="col form-input-col d-none">
 					<!-- <select name="marital" id="marital" class="form-control form_marital" required>
 						<option value="bujang">Bujang</option>
 						<option value="kahwin">Kahwin</option>
@@ -165,7 +165,7 @@
 
 			<div class="form-group row pl-2 mt-3">
 				<label for="education" class="col-form-label col-2 form_education_label">Education Level</label>
-				<div class="col form-input-col">
+				<div class="col form-input-col d-none">
 					<!-- <select name="education" id="education" class="form-control form_education" required>
 						<option value="non-educated">Non-educated</option>
 						<option value="primary">Primary School</option>
@@ -186,7 +186,7 @@
 
 			<div class="form-group row pl-2 mt-3">
 				<label for="occupation" class="col-form-label col-2 form_occupation_label">Occupation</label>
-				<div class="col form-input-col">
+				<div class="col form-input-col d-none">
 					<input type="text" name="occupation" id="occupation" class="form-control form_occupation" value="<?php echo isset($villager->occupation) ? $villager->occupation : ''  ?>" required>
 				</div>
 				<div class="col form-input-div">
@@ -197,7 +197,7 @@
 			<div class="form-group row pl-2 mt-3">
 				<label class="pl-3 padding-top-calc form-radio-label">Adakah anda bermaustatin tetap di alamat ini?</label>
 				<div class="ml-3">
-					<div class="custom-control custom-radio custom-control-inline">
+					<div class="custom-control custom-radio custom-control-inline d-none">
 						<?php if(isset($villager->is_active) && ($villager->is_active == '1')): ?>
 						<input type="radio" id="active_yes" name="active" class="custom-control-input active_yes" value="1" checked required>
 						<?php else: ?>
@@ -206,7 +206,7 @@
 
 						<label class="custom-control-label active_yes_label" for="active_yes">Yes</label>
 					</div>
-					<div class="custom-control custom-radio custom-control-inline">
+					<div class="custom-control custom-radio custom-control-inline d-none">
 						<?php if(isset($villager->is_active) && ($villager->is_active == '0')): ?>
 						<input type="radio" id="active_no" name="active" class="custom-control-input active_no" checked value="0">
 						<?php else: ?>
@@ -234,7 +234,7 @@
 			<div class="form-group row pl-2 mt-3">
 				<label class="pl-3 form-radio-label padding-top-calc">Adakah anda mempunyai tanah yang bergeran?</label>
 				<div class="ml-3">
-					<div class="custom-control custom-radio custom-control-inline">
+					<div class="custom-control custom-radio custom-control-inline d-none">
 						<?php if(isset($villager->is_property_owner) && ($villager->is_property_owner == '1')): ?>
 						<input type="radio" id="propertyOwner_yes" name="propertyOwner" class="custom-control-input propertyOwner_yes" value="1" checked required>
 						<?php else: ?>
@@ -243,7 +243,7 @@
 
 						<label class="custom-control-label propertyOwner_yes_label" for="propertyOwner_yes">Yes</label>
 					</div>
-					<div class="custom-control custom-radio custom-control-inline">
+					<div class="custom-control custom-radio custom-control-inline d-none">
 						<?php if(isset($villager->is_property_owner) && ($villager->is_property_owner == '0')): ?>
 						<input type="radio" id="propertyOwner_no" name="propertyOwner" class="custom-control-input propertyOwner_no" checked value="0">
 						<?php else: ?>
@@ -300,6 +300,11 @@
 	}
 
 	$(document).on("click", "#cancelBtn", function() {
+		var confirm = window.confirm("Confirm to cancel? Changes will be discarded.");
+
+		if (!confirm)
+			return;
+
 		var villager_id = document.getElementById("villager_id").getAttribute("value");
 		$.ajax({
 			type: "GET",
