569ecc7ea1626ab8b9b6d77a68b701c215d220b3
ameliakiew <56687675+ameliakiew@users.noreply.github.com>
2019-10-24 19:26:57 +0800

    border-fixed

--- resources/views/fillForm/step2.blade.php
+++ resources/views/fillForm/step2.blade.php
@@ -3,7 +3,7 @@
 @section('css')
 <style type="text/css">
 	fieldset.scheduler-border {
-	    border: 1px groove #ddd !important;
+	    border: 1px solid #ddd !important;
 	    padding: 0 1.4em 1.4em 1.4em !important;
 	    margin: 0 0 1.5em 0 !important;
 	    -webkit-box-shadow:  0px 0px 0px 0px #000;
