7dcdf40069aa7caa1c5594c3725bca1d39d27e6d
Elvin <hahaelvin@gmail.com>
2019-10-24 22:09:31 +0800

    Statistics page now restricted to logged in user.

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -13,6 +13,11 @@ use App\House;
 
 class StatisticsController extends Controller
 {
+	public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index()
     {
 		return view('statisticsIndex');
