2ce7b5aff4cddb75b19dd670e6a7020aa029b110
tayleechee <56713128+tayleechee@users.noreply.github.com>
2019-11-09 08:09:07 +0800

    add labels for the chart in dashboard page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -104,6 +104,7 @@
                 </div>
             </div>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8"></script>
+            <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels"></script>
             {!! $activeChart->script() !!}
             {!! $genderChart->script() !!}
             {!! $propertyOwnerChart->script() !!}
