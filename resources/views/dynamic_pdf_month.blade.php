75b3b01030d489a850baad3bdddb49d718e2d29e
Elvin <hahaelvin@gmail.com>
2019-11-20 06:24:38 +0800

    Removal of redundant jquery import.

--- resources/views/dynamic_pdf_month.blade.php
+++ resources/views/dynamic_pdf_month.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script> -->
   <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" /> -->
   <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
   <style type="text/css">
